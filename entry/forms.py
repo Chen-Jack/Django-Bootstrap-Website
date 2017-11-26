0d9e7aa77e9691642e76edd03d7e29510ef9462e
Jack C <chen.jack@email.com>
2017-11-26 18:33:53 -0500

    refactor the templates to have less redudant codes

--- entry/forms.py
+++ entry/forms.py
@@ -2,6 +2,7 @@ from django import forms
 from account.models import Entry
 
 class EntryForm(forms.ModelForm):
+    title = forms.CharField(max_length = 50)
     class Meta:
         model = Entry
         fields = ['title', 'text']
